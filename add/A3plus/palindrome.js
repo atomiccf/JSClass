1631946d548b5e9c9198f347160a3ed2485dfc1f
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2022-12-09 22:43:08 +0300

    add minor updates

--- add/A3plus/palindrome.js
+++ add/A3plus/palindrome.js
@@ -3,7 +3,9 @@ function checkPalindrome (str) {
     let str2 = '';
     let clean=str
         .toLowerCase()
-        .replace(/[ёЁЪъЬь,.\s]/gm,'')
+        .replace(/[,.\s]/gm,'')
+        .replace(/ё/g,'е')
+        .replace(/ъ/g,'ь');
         for (let i = clean.length-1; i>=0; i--){
         str2+=clean[i];
 
