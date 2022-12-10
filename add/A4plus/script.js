1e91446ec3f73cd337dca2d465d81fbd375f553f
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2022-12-10 12:04:31 +0300

    add new task

--- add/A4plus/script.js
+++ add/A4plus/script.js
@@ -1,20 +1,34 @@
-let str =prompt('Введите строку')
-function checkPalindromeRecursion () {
+
+
+function runPalindrome(){
+    let str =prompt('Введите строку');
+    checkPalindromeRecursion (str);
+    (checkPalindromeRecursion (str)) ? alert('Это палиндром !') : alert('Это не палиндром !')
+
+
+}
+
+
+function checkPalindromeRecursion (str) {
 
     let clean=str.toLowerCase()
-        .replace(/[ёЁЪъЬь,.\s]/gm,'')
+        .replace(/[,.\s]/gm,'')
+        .replace(/ё/g,'е')
+        .replace(/ъ/g,'ь');
+        let length = clean.length;
+        if (length === 0 || length === 1) {
 
-        if (clean.length === 0 || clean.length === 1) {
+            return true;
 
-            return alert('Это  палиндром!');
+        } else  if (clean[0] === clean[length - 1]) {
 
-        }else if (clean[0] === clean[length - 1]) {
-
-            return checkPalindromeRecursion (clean.slice(1,length-1));
+           return checkPalindromeRecursion (clean.slice(1,length-1));
 
 
         } else {
-            return alert('Это не палиндром!');
+
+
+            return false;
 
 
         }
@@ -27,4 +41,4 @@ function checkPalindromeRecursion () {
 
 
 
-checkPalindromeRecursion ();
+
